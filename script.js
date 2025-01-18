f7774d235cc86da65a3844f99171a0a9727e9b24
rohitchauhan0 <beatfeelrc121@gmail.com>
2025-01-18 10:57:48 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// script.js
+const slider = document.querySelector('.slider');
+let angle = 0;
+
+// Rotate manually using left and right arrow keys
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'ArrowRight') {
+    angle -= 60; // Rotate clockwise
+  } else if (e.key === 'ArrowLeft') {
+    angle += 60; // Rotate counter-clockwise
+  }
+  slider.style.transform = `rotateY(${angle}deg)`;
+});
